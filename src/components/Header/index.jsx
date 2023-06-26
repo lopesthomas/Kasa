ec6dd37ba2e39d1109a4eb7811c94938905e0ce7
Thomas Lopes <thomaslopes001@gmail.com>
2023-06-26 22:59:36 +0200

    Mise en place des routes

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (<div className='header'>
+        <img src={logoo} ></img>
+        {/* <h1 className='kasabanner'>Chez vous, partout et ailleurs</h1> */}
+        <div className='liens'><a href=''>Accueil</a><a href=''>A Propos</a></div>
+        </div>)
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Location from './pages/Location';
+import Error from './components/Error';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App />   
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Location/>}/>
+        <Route path="/" element={<Error/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
